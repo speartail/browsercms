2a765e2137e630342d3a55135118da33523a0ff3
Peter Hoeg <peter@speartail.com>
2014-02-03 19:51:22 +0800

    do not depend on ::Rack::Lock to allow working in threadsafe mode

--- lib/cms/module.rb
+++ lib/cms/module.rb
@@ -22,7 +22,7 @@ module Cms
 
       base.initializer "browsercms.enable_serving_static_assets" do |app|
         # Ensures it is loaded earlier, to avoid blank assets problem listed here: http://jonswope.com/2010/07/25/rails-3-engines-plugins-and-static-assets/
-        app.middleware.insert_before ::Rack::Lock, ::ActionDispatch::Static, "#{root}/public"
+        app.middleware.insert_before ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
       end
 
     end
